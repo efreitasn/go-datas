c1b81704fddf526dd4ea68fcd7c657fccfa6206d
Emanuel Freitas <efreitasn@hotmail.com>
2019-07-23 19:21:18 -0300

    Update linkedlist tests

--- linkedlist/linkedlist_test.go
+++ linkedlist/linkedlist_test.go
@@ -2,41 +2,22 @@ package linkedlist
 
 import "testing"
 
-func TestInsertEnd(t *testing.T) {
+func TestPeekBeginning(t *testing.T) {
 	ll := New()
 
-	ll.InsertEnd("thing1")
-	ll.InsertEnd("thing2")
-	ll.InsertEnd("thing3")
-
-	tail, _ := ll.PeekEnd()
-	expectedTail := "thing3"
-
-	if tail != expectedTail {
-		t.Errorf("got %v, want %v", tail, expectedTail)
-	}
-
-	size := ll.Size()
-	expectedSize := 3
-
-	if size != expectedSize {
-		t.Errorf("got %v, want %v", size, expectedSize)
-	}
-}
-
-func TestDeleteEnd(t *testing.T) {
-	ll := New()
-
+	ll.InsertBeginning("bar")
 	ll.InsertEnd("foo")
-	ll.InsertEnd("bar")
-	ll.InsertEnd("foobar")
-	ll.DeleteEnd()
 
-	tail, _ := ll.PeekEnd()
-	expectedTail := "bar"
+	head, hasHead := ll.PeekBeginning()
+	expectedHead := "bar"
+	expectedHasHead := true
 
-	if tail != expectedTail {
-		t.Errorf("got %v, want %v", tail, expectedTail)
+	if head != expectedHead {
+		t.Errorf("got %v, want %v", head, expectedHead)
+	}
+
+	if hasHead != expectedHasHead {
+		t.Errorf("got %v, want %v", hasHead, expectedHasHead)
 	}
 }
 
@@ -77,4 +58,98 @@ func TestDeleteBeginning(t *testing.T) {
 		t.Errorf("got %v, want %v", head, expectedHead)
 	}
 
+	// Delete when there's only one element in the list
+	ll = New()
+
+	ll.InsertBeginning("bar")
+	ll.DeleteBeginning()
+
+	_, hasTail := ll.PeekEnd()
+	expectedHasTail := false
+	_, hasHead := ll.PeekBeginning()
+	expectedHasHead := false
+
+	if hasTail != expectedHasTail {
+		t.Errorf("got %v, want %v", hasTail, expectedHasTail)
+	}
+
+	if hasHead != expectedHasHead {
+		t.Errorf("got %v, want %v", hasHead, expectedHasHead)
+	}
+}
+
+func TestPeekEnd(t *testing.T) {
+	ll := New()
+
+	ll.InsertEnd("thing1")
+	ll.InsertBeginning("thing2")
+
+	tail, hasTail := ll.PeekEnd()
+	expectedTail := "thing1"
+	expectedHasTail := true
+
+	if tail != expectedTail {
+		t.Errorf("got %v, want %v", tail, expectedTail)
+	}
+
+	if hasTail != expectedHasTail {
+		t.Errorf("got %v, want %v", hasTail, expectedHasTail)
+	}
+}
+
+func TestInsertEnd(t *testing.T) {
+	ll := New()
+
+	ll.InsertEnd("thing1")
+	ll.InsertEnd("thing2")
+	ll.InsertEnd("thing3")
+
+	tail, _ := ll.PeekEnd()
+	expectedTail := "thing3"
+
+	if tail != expectedTail {
+		t.Errorf("got %v, want %v", tail, expectedTail)
+	}
+
+	size := ll.Size()
+	expectedSize := 3
+
+	if size != expectedSize {
+		t.Errorf("got %v, want %v", size, expectedSize)
+	}
+}
+
+func TestDeleteEnd(t *testing.T) {
+	ll := New()
+
+	ll.InsertEnd("foo")
+	ll.InsertEnd("bar")
+	ll.InsertEnd("foobar")
+	ll.DeleteEnd()
+
+	tail, _ := ll.PeekEnd()
+	expectedTail := "bar"
+
+	if tail != expectedTail {
+		t.Errorf("got %v, want %v", tail, expectedTail)
+	}
+
+	// Delete when there's only one element in the list
+	ll = New()
+
+	ll.InsertEnd("bar")
+	ll.DeleteEnd()
+
+	_, hasTail := ll.PeekEnd()
+	expectedHasTail := false
+	_, hasHead := ll.PeekBeginning()
+	expectedHasHead := false
+
+	if hasTail != expectedHasTail {
+		t.Errorf("got %v, want %v", hasTail, expectedHasTail)
+	}
+
+	if hasHead != expectedHasHead {
+		t.Errorf("got %v, want %v", hasHead, expectedHasHead)
+	}
 }
