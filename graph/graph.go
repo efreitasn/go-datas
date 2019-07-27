173c83c890798fd2973130bd72005402f0c82e7a
Emanuel Freitas <efreitasn@hotmail.com>
2019-07-27 19:14:12 -0300

    Fix typos in graph

--- graph/graph.go
+++ graph/graph.go
@@ -1,17 +1,17 @@
-// Package graph provides functions to create/work with graphs of integers
+// Package graph provides functions to create/work with graphs of ints.
 package graph
 
 import (
 	"github.com/efreitasn/go-datas/linkedlist"
 )
 
-// Graph is a graph of integers implemented using an adjacency list
+// Graph is a graph of ints implemented using an adjacency list.
 type Graph struct {
 	adjList  map[int]*linkedlist.LinkedList
 	directed bool
 }
 
-// New creates a graph of integers
+// New creates a graph of ints.
 func New(directed bool) *Graph {
 	return &Graph{
 		adjList:  map[int]*linkedlist.LinkedList{},
@@ -19,24 +19,38 @@ func New(directed bool) *Graph {
 	}
 }
 
-// IsDirected checks whether the graph is directed
+// IsDirected checks whether the graph is directed.
 func (g *Graph) IsDirected() bool {
 	return g.directed
 }
 
-// AddVertex adds a vertex to the graph
-func (g *Graph) AddVertex(v int) {
-	g.adjList[v] = linkedlist.New()
-}
-
-// HasVertex checks whether the graph contains a vertex
+// HasVertex checks whether the graph contains a vertex.
 func (g *Graph) HasVertex(v int) bool {
 	_, found := g.adjList[v]
 
 	return found
 }
 
-// AddEdge adds an edge to the graph
+// AddVertex adds a vertex to the graph.
+func (g *Graph) AddVertex(v int) {
+	g.adjList[v] = linkedlist.New()
+}
+
+// VertexDegree returns the degree of a vertex in the graph.
+func (g *Graph) VertexDegree(v int) (degree int, found bool) {
+	if !g.HasVertex(v) {
+		return 0, false
+	}
+
+	return g.adjList[v].Size(), true
+}
+
+// NumVertices returns the number of vertices in the graph.
+func (g *Graph) NumVertices() int {
+	return len(g.adjList)
+}
+
+// AddEdge adds an edge to the graph.
 func (g *Graph) AddEdge(v int, v2 int) bool {
 	if !g.HasVertex(v) || !g.HasVertex(v2) {
 		return false
@@ -51,21 +65,7 @@ func (g *Graph) AddEdge(v int, v2 int) bool {
 	return true
 }
 
-// VertexDegree returns the degree of a vertex in the graph
-func (g *Graph) VertexDegree(v int) (degree int, found bool) {
-	if !g.HasVertex(v) {
-		return 0, false
-	}
-
-	return g.adjList[v].Size(), true
-}
-
-// NumVertices returns the number of vertices in the graph
-func (g *Graph) NumVertices() int {
-	return len(g.adjList)
-}
-
-// NumEdges returns the number of edges in the graph
+// NumEdges returns the number of edges in the graph.
 func (g *Graph) NumEdges() int {
 	var n int
 
